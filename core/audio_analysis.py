cdbfe3763584da603ca7ed39ebb62439d4dc0c78
ArdianDW <ardiandwic37@gmail.com>
2025-09-10 23:08:27 +0700

    update aaudio analysis

--- core/audio_analysis.py
+++ core/audio_analysis.py
@@ -1,33 +1,37 @@
 import librosa
 import numpy as np
 
-def detect_audio_jumpscares(audio_path: str, sr: int = 22050) -> list[float]:
-    y, sr = librosa.load(audio_path, sr=sr)
-
-    hop_length = 512
-    frame_length = 1024
-    rms = librosa.feature.rms(y=y, frame_length=frame_length, hop_length=hop_length)[0]
-
-    stft = np.abs(librosa.stft(y, n_fft=2048, hop_length=hop_length))
-    freqs = librosa.fft_frequencies(sr=sr, n_fft=2048)
-    bass_band = (freqs >= 20) & (freqs <= 200)
-    bass_energy = stft[bass_band].mean(axis=0)
-
-    rms = (rms - rms.min()) / (rms.max() - rms.min() + 1e-9)
-    bass_energy = (bass_energy - bass_energy.min()) / (bass_energy.max() - bass_energy.min() + 1e-9)
-
-    rms_diff = np.diff(rms, prepend=rms[0])
-    bass_diff = np.diff(bass_energy, prepend=bass_energy[0])
-
+def detect_audio_jumpscares(audio_path: str, sr: int = 22050, chunk_duration: int = 1800) -> list[float]:
+    total_duration = librosa.get_duration(filename=audio_path)
     jumpscare_times = []
-    cooldown = 5.0  
+    cooldown = 5.0
     last_detect = -cooldown
 
-    for i in range(len(rms)):
-        time = librosa.frames_to_time(i, sr=sr, hop_length=hop_length)
-        if rms_diff[i] > 0.21 and bass_diff[i] > 0.12:
-            if time - last_detect >= cooldown:
-                jumpscare_times.append(time)
-                last_detect = time
+    hop_length = 2048
+    frame_length = 4096
+
+    for chunk_start in np.arange(0, total_duration, chunk_duration):
+        y, _ = librosa.load(audio_path, sr=sr, offset=chunk_start, duration=chunk_duration)
+        if len(y) == 0:
+            continue
+
+        rms = librosa.feature.rms(y=y, frame_length=frame_length, hop_length=hop_length)[0]
+        stft = np.abs(librosa.stft(y, n_fft=2048, hop_length=hop_length))
+        freqs = librosa.fft_frequencies(sr=sr, n_fft=2048)
+        bass_band = (freqs >= 20) & (freqs <= 200)
+        bass_energy = stft[bass_band].mean(axis=0)
+
+        rms = (rms - rms.min()) / (rms.max() - rms.min() + 1e-9)
+        bass_energy = (bass_energy - bass_energy.min()) / (bass_energy.max() - bass_energy.min() + 1e-9)
+
+        rms_diff = np.diff(rms, prepend=rms[0])
+        bass_diff = np.diff(bass_energy, prepend=bass_energy[0])
+
+        for i in range(len(rms)):
+            time = chunk_start + librosa.frames_to_time(i, sr=sr, hop_length=hop_length)
+            if rms_diff[i] > 0.21 and bass_diff[i] > 0.12:
+                if time - last_detect >= cooldown:
+                    jumpscare_times.append(time)
+                    last_detect = time
 
     return jumpscare_times
